4f4ef2ca513550425cf877a945cca8c41ee553f9
msnaye <99111936+msnaye@users.noreply.github.com>
2022-06-21 00:34:58 -0500

    cleaned up code

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+router.use(require('./candidateRoutes'));
+
+
+router.use(require('./partyRoutes'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
