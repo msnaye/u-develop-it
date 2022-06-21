2351f9a49c2ca1310403fcb84d51bb6bb51ff783
msnaye <99111936+msnaye@users.noreply.github.com>
2022-06-21 01:05:11 -0500

    added routes for voter

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.use(require('./candidateRoutes'));
-
-
 router.use(require('./partyRoutes'));
+router.use(require('./voterRoutes'));
 
 
 
